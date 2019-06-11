3be9172f937b39cb51bff432c2418de4c38eb699
shijnasurendran <shijnasurendran@gmail.com>
2019-06-11 10:58:02 -0400

    movie fun app modernization

--- applications/movie-fun-app/src/main/java/org/superbiz/moviefun/Application.java
+++ applications/movie-fun-app/src/main/java/org/superbiz/moviefun/Application.java
@@ -20,7 +20,6 @@ public class Application {
     }
 
     @Bean
-
     public ServletRegistrationBean actionServletRegistration(MovieServlet movieServlet) {
 
         return new ServletRegistrationBean(movieServlet, "/moviefun/*");
